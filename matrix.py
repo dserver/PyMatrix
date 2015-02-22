4905d645eb3b25bb552b14981519e02b29668688
dserver <jonathanhenk@gmail.com>
2015-02-21 17:01:41 -0800

    first commit

---
+++ matrix.py
@@ -0,0 +1,33 @@
+
+class SimpleMatrix:
+   def __init__(self, matrix):
+      self._matrix = matrix
+      
+   def exchange_rows(self, row1, row2):
+      row1 -= 1
+      row2 -= 1
+      temp = []
+      for i in range(len(self._matrix[row1])):
+         temp.append(self._matrix[row1][i])
+      self._matrix[row1] = self._matrix[row2]
+      self._matrix[row2] = temp
+
+   def print_matrix(self):
+      for r in range(len(self._matrix)):
+         row_string = ""
+         for c in range(len(self._matrix[r])):
+            row_string += str(self._matrix[r][c]) + ", "
+
+         print row_string
+
+   def multiply_row_by_const(self, row, const):
+      row -=1
+      for c in range(len(self._matrix[row])):
+         newval = float(self._matrix[row][c]) * const
+         self._matrix[row][c] = newval
+
+matrix = [[1.0, 2.0, 3.0], [4.0, 5.0, 6.0], [7.0, 8.0, 9.0]]
+simplematrix = SimpleMatrix(matrix)
+simplematrix.exchange_rows(1, 2)
+simplematrix.multiply_row_by_const(3, 1.0/7.0)
+simplematrix.print_matrix()
